06a2217b67b9502df525cfe4e28a5da8161d5d9e
Dan Alcantara <96092902+dan-alcantara-neeva@users.noreply.github.com>
2022-05-31 21:41:49 -0700

    Enable leak canary and StrictMode checks (#665)

--- app/src/main/java/com/neeva/app/NeevaBrowser.kt
+++ app/src/main/java/com/neeva/app/NeevaBrowser.kt
@@ -1,7 +1,30 @@
 package com.neeva.app
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NeevaBrowser : Application()
+class NeevaBrowser : Application() {
+    override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build()
+            )
+        }
+
+        super.onCreate()
+    }
+}
