1ed1359df7b82d3a1f3a26d11d3cc8fc59e64bec
Dan Alcantara <96092902+dan-alcantara-neeva@users.noreply.github.com>
2022-07-18 13:32:00 -0700

    Remove 'below' to make the directions toolbar agnostic (#811)
    * Remove the word "below" from the string on the empty tab page so that user isn't looking in the wrong place
    * Fix the screenshot taking script so that CircleCI doesn't disagree with it again

--- scripts/create-emulator.sh
+++ scripts/create-emulator.sh
@@ -12,6 +12,7 @@ sdkmanager "system-images;android-28;default;$ARCH"
 echo "no" | avdmanager --verbose create avd -n "Pixel2Avd" -k "system-images;android-28;default;$ARCH" -d "pixel_2"
 
 # Allow the developer's PC keyboard to work on the emulator.
+perl -p -i -e "s/hw\.keyboard = false/hw.keyboard = true/g" ~/.android/avd/Pixel2Avd.avd/config.ini
 perl -p -i -e "s/hw\.keyboard = false/hw.keyboard = true/g" ~/.android/avd/Pixel2Avd.avd/hardware-qemu.ini
 
-emulator -avd "Pixel2Avd" -no-audio -no-boot-anim -verbose -no-snapshot -partition-size 2048
+emulator -avd "Pixel2Avd" -no-audio -no-boot-anim -verbose -no-snapshot -gpu swiftshader_indirect -partition-size 2048
