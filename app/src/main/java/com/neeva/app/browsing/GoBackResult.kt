06afa3632869e05cb107a682f6955742ada29a37
Darin Fisher <79867437+darinwf@users.noreply.github.com>
2022-09-01 16:14:24 -0700

    Add license headers (#942)

--- app/src/main/java/com/neeva/app/browsing/GoBackResult.kt
+++ app/src/main/java/com/neeva/app/browsing/GoBackResult.kt
@@ -1,3 +1,7 @@
+// Copyright 2022 Neeva Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package com.neeva.app.browsing
 
 /** Data needed to process what happens when a user hits back while actively viewing a webpage. */
