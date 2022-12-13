7c5d108f46039935ab7db6135036595683f4a994
Dan Alcantara <96092902+dan-alcantara-neeva@users.noreply.github.com>
2022-12-12 17:29:05 -0800

    Strings update 2022-12-09 (#1141)
    * Fix string script
    
    * Update the strings

--- scripts/string_updater.sh
+++ scripts/string_updater.sh
@@ -3,10 +3,10 @@
 OUTPUT_DIR=/tmp/`date "+%y-%m-%d-%H-%M"`
 echo $OUTPUT_DIR
 
-unzip $1 -d $OUTPUT_DIR
+unzip "$1" -d "$OUTPUT_DIR"
 
-mv $OUTPUT_DIR/en-GB/strings.xml app/src/main/res/values-en-rGB/strings.xml
-mv $OUTPUT_DIR/de-DE/strings.xml app/src/main/res/values-de-rDE/strings.xml
-mv $OUTPUT_DIR/fr-FR/strings.xml app/src/main/res/values-fr-rFR/strings.xml
+mv "$OUTPUT_DIR/en-GB/strings.xml" "app/src/main/res/values-en-rGB/strings.xml"
+mv "$OUTPUT_DIR/de-DE/strings.xml" "app/src/main/res/values-de-rDE/strings.xml"
+mv "$OUTPUT_DIR/fr-FR/strings.xml" "app/src/main/res/values-fr-rFR/strings.xml"
 
 # TODO(danalcantara): Add Spanish support once we start shipping those strings.
